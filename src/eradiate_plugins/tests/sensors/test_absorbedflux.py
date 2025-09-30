41b594efb4f5e6f7b5c58f2a9af2a5ee6ec3c9da
nicolae marton <nicolae.marton@gmail.com>
2025-09-30 11:23:29 +0200

    add absorbed flux test for resolution higher than 1x1x1

--- src/eradiate_plugins/tests/sensors/test_absorbedflux.py
+++ src/eradiate_plugins/tests/sensors/test_absorbedflux.py
@@ -83,16 +83,50 @@ def test_closed_box(variant_scalar_rgb):
 
     scene = closed_box_scene(0.3, [0.,0.,-1])
     res = mi.render(scene, spp=1)
-    print(res.numpy().squeeze())
     assert np.allclose(res.numpy().squeeze(), 1.)
 
     scene = closed_box_scene(0., [-0.5,0.,-0.5])
     res = mi.render(scene, spp=1)
-    print(res.numpy().squeeze(), np.cos(np.deg2rad(45)))
     # assert np.allclose(res.numpy().squeeze(), 1.)
     assert np.allclose(res.numpy().squeeze(), np.cos(np.deg2rad(45)))
 
     scene = closed_box_scene(0.3, [-0.5,0.,-0.5])
     res = mi.render(scene, spp=1)
-    print(res.numpy().squeeze(), np.cos(np.deg2rad(45)))
     assert np.allclose(res.numpy().squeeze(), np.cos(np.deg2rad(45)))
+
+
+def test_absorbed_voxels(variant_scalar_rgb):
+    scene = mi.load_dict({
+        "type": "scene",
+        "sensor": {
+            "type":"absorbedflux",
+            "bbox_min":[-1.,-1, -0.1],
+            "bbox_max":[1.,1.,1.],
+            "film": {
+                "type": "volfilm",
+                "res_x": 2,
+                "res_y": 1,
+                "res_z": 3,
+            },
+        },
+        "integrator": { "type": "paccumulator", },
+        "surface": {
+            "type": "rectangle",
+            "bsdf":{ "type":"diffuse", "reflectance":0.7, },
+        },
+        "light": {
+            "type": "directionalperiodic",
+            "pbox_min": [0.,-1, -0.1],
+            "pbox_max": [1.,1.,1.],
+            "irradiance": 1.0,
+            "direction": [0.,0.,-1.],
+        },
+    })
+
+    res = mi.render(scene, spp=1)
+    gt_res = np.zeros((2,1,3,1))
+    gt_res[1,0,0] = 0.3 * 2
+    
+    assert np.allclose(res.numpy(), gt_res)
+
+ 
